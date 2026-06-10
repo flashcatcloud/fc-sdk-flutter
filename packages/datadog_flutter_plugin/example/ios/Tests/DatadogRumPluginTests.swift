dfd6682ca13f7159a3f97ed069ff9631b0893e8e
Jeff Ward <jeff.ward@datadoghq.com>
2026-06-10 13:49:02 -0400

    chore(ci): Fix two flakey tests
    The RUM plugin test checking reporting LongTasks could fail with float precision issues, so allow it to check with some amount of tolerance. Also use a specific value instead of DateTime.now
    
    Fix a potential for an out of bounds read in zlib decode that could affect any test if they were run in parallel

--- packages/datadog_flutter_plugin/example/ios/Tests/DatadogRumPluginTests.swift
+++ packages/datadog_flutter_plugin/example/ios/Tests/DatadogRumPluginTests.swift
@@ -698,8 +698,7 @@ class DatadogRumPluginTests: XCTestCase {
     }
 
     func testReportLongTask_CallsInternal() {
-        let startTime = Date.now
-        let startTimeInterval = startTime.timeIntervalSince1970
+        let startTimeInterval: TimeInterval = 1_000_000.0
         let duration = 340124
 
         let call = FlutterMethodCall(methodName: "reportLongTask", arguments: [
@@ -714,10 +713,8 @@ class DatadogRumPluginTests: XCTestCase {
 
         let command = mock.commands.first as? RUMAddLongTaskCommand
         XCTAssertNotNil(command)
-        XCTAssertEqual(command, RUMAddLongTaskCommand(time: Date(timeIntervalSince1970: startTimeInterval),
-                                                      attributes: [:],
-                                                      duration: TimeInterval(Double(duration) / 1000.0))
-        )
+        XCTAssertEqual(command?.time.timeIntervalSince1970, startTimeInterval, accuracy: 0.001)
+        XCTAssertEqual(command?.duration, TimeInterval(Double(duration) / 1000.0), accuracy: 0.0001)
         XCTAssertEqual(resultStatus, .called(value: nil))
     }
 
